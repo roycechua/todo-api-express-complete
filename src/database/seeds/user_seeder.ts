278ca29bc14f8a1f7d74007de81600b1859df6a4
Royce <royce236@gmail.com>
2024-02-27 23:34:35 +0800

    Add initial seeder

---
+++ src/database/seeds/user_seeder.ts
@@ -0,0 +1,15 @@
+import { hash } from "bcrypt";
+import { Knex } from "knex";
+
+export async function seed(knex: Knex): Promise<void> {
+    // Deletes ALL existing entries
+    await knex("users").del();
+
+    const hashedPassword = await hash('dummy', 10);
+    // Inserts seed entries
+    await knex("users").insert([
+        { id: 1, email: "test1@gmail.com", password: hashedPassword },
+        { id: 2, email: "test2@gmail.com", password: hashedPassword },
+        { id: 3, email: "test3@gmail.com", password: hashedPassword }
+    ]);
+};
