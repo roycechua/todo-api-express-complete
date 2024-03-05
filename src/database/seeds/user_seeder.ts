2a417db83b09528dedce8908a31ac8c5b6646aa6
Royce <royce236@gmail.com>
2024-03-05 22:30:33 +0800

    Update default dummy password

--- src/database/seeds/user_seeder.ts
+++ src/database/seeds/user_seeder.ts
@@ -5,7 +5,7 @@ export async function seed(knex: Knex): Promise<void> {
     // Deletes ALL existing entries
     await knex("users").del();
 
-    const hashedPassword = await hash('dummy', 10);
+    const hashedPassword = await hash('test123456', 10);
     // Inserts seed entries
     await knex("users").insert([
         { id: 1, email: "test1@gmail.com", password: hashedPassword },
