2e5bf0abb37576d4d70c5b75594e6f4a0f4f4a66
Royce <royce236@gmail.com>
2024-03-13 00:14:25 +0800

    Update todo migration file

--- src/database/migrations/20240312153520_add_todo_table.ts
+++ src/database/migrations/20240312153520_add_todo_table.ts
@@ -1,13 +1,14 @@
 import { Knex } from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.table('todos', table => {
+  return knex.schema.createTable('todos', table => {
     table.bigIncrements('id').unsigned().primary();
     table.integer('userId').unsigned();
     table.string('title');
     table.string('description');
     table.boolean('completed').defaultTo(false);
-    table.timestamp('created_at').defaultTo(knex.fn.now);
+    table.timestamp('created_at').defaultTo(knex.fn.now());
+    table.timestamp('updated_at').defaultTo(knex.fn.now());
   });
 }
 
